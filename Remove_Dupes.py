aad31cece7c42b733de4107e61ec5ec47305b5ab
Brendan Price <56489848+B33ndon@users.noreply.github.com>
2020-12-15 01:24:45 +0000

    Add files via upload

---
+++ Remove_Dupes.py
@@ -0,0 +1,12 @@
+
+
+def remove_duplicates(lista):
+    lista2 = []
+    if lista:
+        for item in lista:
+            if item not in lista2:
+                lista2.append(item)
+    else:
+        return lista
+    return lista2
+print (remove_duplicates([1,2,3,3]))
